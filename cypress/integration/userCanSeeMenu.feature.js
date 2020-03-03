7135b2c217b2271b4c0b4a959c3cdec9c9606a88
karolina <karolina.frostare@gmail.com>
2020-03-03 13:53:25 +0100

    creates user can see menu test

---
+++ cypress/integration/userCanSeeMenu.feature.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('user views menus', () => {
+  beforeEach(() => {
+    cy.visit("/");
+  })
+  it('successfully', () => {
+    cy.get('#index').within(() => {
+      cy.contains('Spaghetti and Meatballs')
+      cy.contains('Salad')
+      cy.contains('Ice Cream')
+    })
+  })
+
+  it('unsuccessfully', () => {
+      cy.get('#index').should('not.exist')
+  })
+
+})

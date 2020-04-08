e83ded6a29bbe0dde23c10006107c340f285f39f
faraznaeem <faraznaeem@gmail.com>
2020-04-08 13:59:19 +0200

    Adds axios and implementation code from 01

--- src/components/DisplayProductData.jsx
+++ src/components/DisplayProductData.jsx
@@ -1,39 +1,56 @@
-import React, { Component } from 'react';
-import { getData } from '../modules/productData';
+import React, { Component } from "react";
+import { getData } from "../modules/productData";
+import axios from 'axios'
 
 class DisplayProductData extends Component {
-	state = {
-		productData: []
-	}
+  state = {
+    productData: [],
+  };
 
-	componentDidMount() {
-		this.getProductData()
-	}
+  componentDidMount() {
+    this.getProductData();
+  }
 
-	async getProductData() {
-		let result = await getData();
-		this.setState({ productData: result.data.products })
-	}
+  async getProductData() {
+    let result = await getData();
+    this.setState({ productData: result.data.products });
+  }
 
-	render() {
-		let dataIndex
-		debugger
-		if (Array.isArray(this.state.productData) && this.state.productData.length ) {
-			dataIndex = (
-				<div id="index">
-					{this.state.productData.map(item => {
-						return <div key={item.id}>{item.name}{item.description}{item.price}</div>
-					})}
-				</div>
-			)
-		} 
+  async addToOrder(event) {
+    let id = event.target.parentElement.dataset.id
+    let result = await axios.post('http://localhost:3000/api/orders', { id: id } )
+  }
 
-		return (
-			<div>
-				{dataIndex}
-			</div>
-		)
-	}
+  render() {
+    let dataIndex;
+
+    if (
+      Array.isArray(this.state.productData) &&
+      this.state.productData.length
+    ) {
+      dataIndex = (
+        <div id="index">
+          {this.state.productData.map((item) => {
+            return (
+              <div
+                key={item.id}
+                id={`product-${item.id}`}
+                data-id={item.id}
+                data-price={item.price}
+              >
+                {`${item.name} ${item.description} ${item.price}`}
+                <button onClick={this.addToOrder.bind(this)}>
+                  Add to order
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+
+    return <div>{dataIndex}</div>;
+  }
 }
 
 export default DisplayProductData;

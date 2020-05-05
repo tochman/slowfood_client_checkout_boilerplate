8fa32bdb40f0c68e053c0ab1dbc6ca23105695de
faraznaeem <faraznaeem@gmail.com>
2020-05-05 15:55:26 +0200

    Adds a Add to order button

--- src/components/DisplayProductData.jsx
+++ src/components/DisplayProductData.jsx
@@ -13,16 +13,28 @@ class DisplayProductData extends Component {
 	async getProductData() {
 		let result = await getData();
 		this.setState({ productData: result.data.products })
-	}
+  }
+  
+  addToOrder() {
+    
+  }
 
 	render() {
 		let dataIndex
-		debugger
 		if (Array.isArray(this.state.productData) && this.state.productData.length ) {
 			dataIndex = (
 				<div id="index">
 					{this.state.productData.map(item => {
-						return <div key={item.id}>{item.name}{item.description}{item.price}</div>
+						return (
+              <div key={item.id}>
+                {item.name}
+                {item.description}
+                {item.price}
+                <button onClick={this.addToOrder.bind(this)}>
+                  Add to order
+                </button>
+              </div>
+            );
 					})}
 				</div>
 			)

faeeed91d0953bff6ca582c4f20dbd4101011786
Thomas <thomas@craftacademy.se>
2020-11-25 11:00:48 +0100

    code formatting

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,24 @@ import React, { Component } from 'react';
 import DisplayProductData from './components/DisplayProductData';
 import Login from './components/Login'
 
-// const App = () => {
-// 	return (
-// 		<>
-// 			<Login />
-// 			<h1>Slowfood</h1>
-// 			<DisplayProductData />
-// 		</>
-// 	);
-// }
-
 class App extends Component {
+  state = {
+    authenticated: false
+  }
 
-	state = {
-		authenticated: false
-	}
+  toggleAuthenticatedState() {
+    this.setState({ authenticated: !this.state.authenticated })
+  }
 
-	toggleAuthenticatedState() {
-		this.setState({authenticated: !this.state.authenticated})
-	}
-
-	render() {
-		return (
-			<>
-				<Login toggleAuthenticatedState={() => this.toggleAuthenticatedState()}/>
-				<h1>Slowfood</h1>
-				<DisplayProductData />
-			</>
-		)
-	}
+  render() {
+    return (
+      <>
+        <Login toggleAuthenticatedState={() => this.toggleAuthenticatedState()} />
+        <h1>Slowfood</h1>
+        <DisplayProductData />
+      </>
+    )
+  }
 }
 
 export default App;
--- src/components/DisplayProductData.jsx
+++ src/components/DisplayProductData.jsx
@@ -2,44 +2,44 @@ import React, { Component } from 'react';
 import { getData } from '../modules/productData';
 
 class DisplayProductData extends Component {
-	state = {
-		productData: []
-	}
+  state = {
+    productData: []
+  }
 
-	componentDidMount() {
-		this.getProductData()
-	}
+  componentDidMount() {
+    this.getProductData()
+  }
 
-	async getProductData() {
-		let result = await getData();
-		this.setState({ productData: result.data.products })
-	}
+  async getProductData() {
+    let result = await getData();
+    this.setState({ productData: result.data.products })
+  }
 
-	render() {
-		let dataIndex
-		if (Array.isArray(this.state.productData) && this.state.productData.length) {
-			dataIndex = (
-				<div id="index">
-					{this.state.productData.map(item => {
-						return (
-							<div key={item.id} data-cy={`product-${item.id}`}>
-								{item.name}{item.description}{item.price}
-								{ localStorage.getItem('authenticated') === 'true' &&
-									<button>Add to Order</button>
-								}
-							</div>
-						)
-					})}
-				</div>
-			)
-		}
+  render() {
+    let dataIndex
+    if (Array.isArray(this.state.productData) && this.state.productData.length) {
+      dataIndex = (
+        <div id="index">
+          {this.state.productData.map(item => {
+            return (
+              <div key={item.id} data-cy={`product-${item.id}`}>
+                {item.name}{item.description}{item.price}
+                { localStorage.getItem('authenticated') === 'true' &&
+                  <button>Add to Order</button>
+                }
+              </div>
+            )
+          })}
+        </div>
+      )
+    }
 
-		return (
-			<div>
-				{dataIndex}
-			</div>
-		)
-	}
+    return (
+      <>
+        {dataIndex}
+      </>
+    )
+  }
 }
 
 export default DisplayProductData;
